96b1e9f6e581fc513d58de18e5457b6f9dbe59a9
young-kim <ceo.youngkim@gmail.com>
2024-02-05 16:23:27 +0900

    readme 업데이트

---
+++ README.md
@@ -0,0 +1,10 @@
+## 자동배포 게시판 서비스 구축
+
+### 목표
+Kotlin(1.9.22), SpringBoot(3.2.2) 환경에서 Github Actions을 활용한 AWS 자동배포 게시판 서비스를 구축한다.
+
+### 과정
+
+---
+
+참고강의 - [패스트캠퍼스 코틀린 강의](https://fastcampus.co.kr/courses/217930)
--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
